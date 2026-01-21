ded8d361455c577806d59ee42d55ec0065585402
Tyler <tjcrimando@gmail.com>
2026-01-20 23:07:03 -0700

    added square outline and different color mode. Still working on filled square which is giving me scaling issues

--- interactiveFibonacci.js
+++ interactiveFibonacci.js
@@ -2,7 +2,12 @@ var canvas;
 var gl;
 var justOne = 0;
 
-let vertices = generateVertexListForFibList()
+//let vertices = generateDiagonal()
+let vertices = generateSquareOutline()
+//let vertices = generateSquareFilled()
+//let vertices = generateCurve()
+
+
 
 function generateFibArray(size){
     let retlist = [1, 1]
@@ -25,10 +30,27 @@ function makeColorsFromVertices(verts){
     return colors;
 }
 
+function makeFilledColorsFromVertices(verts){
+    let colors = [];
+
+    for (let i = 0; i < verts.length; i += 6){
+        const r = Math.random();
+        const g = Math.random();
+        const b = Math.random();
+    
+        for (let j = 0; j < 6; j++){
+            colors.push(vec3(r,g,b));
+        }
+
+    }
+
+    return colors;
+}
+
+
 //takes values of array (assumed to be nums) and takes their sum, and creates a new normalized array
 function normalizeArray(inArray){
     
-    let side;
     let retArray = [];
     let total = 0;
     for(let i = inArray.length-1; i >= 0; i--){
@@ -36,7 +58,6 @@ function normalizeArray(inArray){
     }
 
     for(let i = inArray.length-1; i > 0; i--){
-        side = inArray[i] + inArray[i-1];
         let distance = (inArray[i]/total);
         retArray.push(2*distance);
     }
@@ -44,13 +65,13 @@ function normalizeArray(inArray){
     return retArray;
 }
 
-function generateVertexListForFibList(size = 11){
+function generateDiagonal(size = 11){
     let vertices = [vec2(-1,-1)];
     let lastX = -1;
     let lastY = -1;
     let fibArray = normalizeArray(generateFibArray(size));
 
-    console.log("attempting to generate...")
+    console.log("attempting to generate diagonals...")
     let direction = 0;
     for (let i = 0; i < fibArray.length; i++){
         if(direction == 0){
@@ -84,22 +105,151 @@ function generateVertexListForFibList(size = 11){
     return vertices;
 }
 
+function generateSquareOutline(size = 11){
+    let vertices = [];
+    let lastX = -1;
+    let lastY = -1;
+    let fibArray = normalizeArray(generateFibArray(size));
+    let mode = 0; //0 is bl, 1 is tl, 2 is tr, 3 is br
+
+    for (let i = 0; i < fibArray.length; i++){
+        let bl;
+        let br;
+        let tl;
+        let tr;
+        if ( mode == 0 ){
+            bl = vec2(lastX, lastY);
+            br = vec2(lastX+fibArray[i], lastY);
+            tl = vec2(lastX, lastY+fibArray[i]);
+            tr = vec2(lastX+fibArray[i], lastY+fibArray[i]); 
+            lastX = lastX+fibArray[i];
+            lastY = lastY+fibArray[i];
+        }
+        else if ( mode == 1 ){
+            bl = vec2(lastX, lastY-fibArray[i]);
+            br = vec2(lastX+fibArray[i], lastY-fibArray[i]);
+            tl = vec2(lastX, lastY);
+            tr = vec2(lastX+fibArray[i], lastY);
+            lastX = lastX+fibArray[i];
+            lastY = lastY-fibArray[i];
+        }
+        else if ( mode == 2 ){
+            bl = vec2(lastX-fibArray[i], lastY-fibArray[i]);
+            br = vec2(lastX, lastY-fibArray[i]);
+            tl = vec2(lastX-fibArray[i], lastY);
+            tr = vec2(lastX, lastY);
+            lastX = lastX-fibArray[i];
+            lastY = lastY-fibArray[i];
+        }
+        else {
+            bl = vec2(lastX - fibArray[i], lastY);
+            br = vec2(lastX, lastY);
+            tl = vec2(lastX - fibArray[i], lastY + fibArray[i]);
+            tr = vec2(lastX, lastY + fibArray[i]);
+            lastX -= fibArray[i];
+            lastY += fibArray[i];
+        }
+        mode = (mode+1)%4;
+        vertices.push(...[bl, br, tr, tl]);
+    }
+
+    vertices = stetchToFitFib(vertices);
+    return vertices;
+}
+
+function generateSquareFilled(size = 11){
+    let vertices = [];
+    let lastX = -1;
+    let lastY = -1;
+    let fibArray = normalizeArray(generateFibArray(size));
+    let mode = 0; //0 is bl, 1 is tl, 2 is tr, 3 is br
+
+    for (let i = 0; i < fibArray.length; i++){
+        let bl;
+        let br;
+        let tl;
+        let tr;
+        if ( mode == 0 ){
+            bl = vec2(lastX, lastY);
+            br = vec2(lastX+fibArray[i], lastY);
+            tl = vec2(lastX, lastY+fibArray[i]);
+            tr = vec2(lastX+fibArray[i], lastY+fibArray[i]); 
+            lastX = lastX+fibArray[i];
+            lastY = lastY+fibArray[i];
+        }
+        else if ( mode == 1 ){
+            bl = vec2(lastX, lastY-fibArray[i]);
+            br = vec2(lastX+fibArray[i], lastY-fibArray[i]);
+            tl = vec2(lastX, lastY);
+            tr = vec2(lastX+fibArray[i], lastY);
+            lastX = lastX+fibArray[i];
+            lastY = lastY-fibArray[i];
+        }
+        else if ( mode == 2 ){
+            bl = vec2(lastX-fibArray[i], lastY-fibArray[i]);
+            br = vec2(lastX, lastY-fibArray[i]);
+            tl = vec2(lastX-fibArray[i], lastY);
+            tr = vec2(lastX, lastY);
+            lastX = lastX-fibArray[i];
+            lastY = lastY-fibArray[i];
+        }
+        else {
+            bl = vec2(lastX - fibArray[i], lastY);
+            br = vec2(lastX, lastY);
+            tl = vec2(lastX - fibArray[i], lastY + fibArray[i]);
+            tr = vec2(lastX, lastY + fibArray[i]);
+            lastX -= fibArray[i];
+            lastY += fibArray[i];
+        }
+        mode = (mode+1)%4;
+        vertices.push(...[bl, br, tr, bl, tr, tl]);
+    }
+
+    vertices = stetchToFitFib(vertices);
+    return vertices;
+}
+
+function generateCurve(depth = 8){
+
+}
+
+function filledStretch(vecarray){
+    let x_max = -Infinity;
+    let y_max = -Infinity;
+}
+
 //I'm confident there is in fact a more mathematical / less hacky way to go about this. But I'm a computer scientist as
 //much as I am a math teacher.
 function stetchToFitFib(vecarray){
-    const topY   = vecarray[1][1];
-    const rightX = vecarray[2][0];
-
-    const sx = 2 / (rightX + 1);
-    const sy = 2 / (topY + 1);
+    let minX = Infinity, maxX = -Infinity, minY = Infinity, maxY = -Infinity;
 
     for (let i = 0; i < vecarray.length; i++){
-        vecarray[i][0] = -1 + sx * (vecarray[i][0] + 1);
-        vecarray[i][1] = -1 + sy * (vecarray[i][1] + 1);
+        const x = vecarray[i][0];
+        const y = vecarray[i][1];
+        if (x < minX) minX = x;
+        if (x > maxX) maxX = x;
+        if (y < minY) minY = y;
+        if (y > maxY) maxY = y;
+    }
+
+    const w = (maxX - minX) || 1;
+    const h = (maxY - minY) || 1;
+
+    const sx = 2 / w;
+    const sy = 2 / h;
+
+    for (let i = 0; i < vecarray.length; i++){
+        if (vecarray[i][0] !== -1){ 
+        vecarray[i][0] = -1 + (vecarray[i][0] - minX) * sx;}
+        
+        if (vecarray[i][1] !== -1){
+        vecarray[i][1] = -1 + (vecarray[i][1] - minY) * sy;}
     }
 
     return vecarray;
 }
+
+
 
 window.onload = function init()
 {
@@ -120,6 +270,7 @@ window.onload = function init()
     gl.enableVertexAttribArray(vPosition);
 
     var colors = makeColorsFromVertices(vertices);
+    //var colors = makeFilledColorsFromVertices(vertices); //for filled squares
 
     var colorBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
@@ -129,13 +280,32 @@ window.onload = function init()
     gl.vertexAttribPointer(vColor, 3, gl.FLOAT, false, 0, 0);
     gl.enableVertexAttribArray(vColor);
 
+    for (let i = 0; i < vertices.length; i++){
+        console.log(`${vertices[i][0]} : ${vertices[i][1]}`);
+    }
+
     render();
 }
 
 function render() {
-    
-
 
     gl.clear( gl.COLOR_BUFFER_BIT );
+    //drawDiagonals();
+    drawSquareOutlines();
+}
+
+function drawDiagonals(){
     gl.drawArrays(gl.LINE_STRIP, 0, vertices.length);
+}
+
+function drawSquareOutlines(){
+    
+    for (let i = 0; i < vertices.length; i += 4) {
+        gl.drawArrays(gl.LINE_LOOP, i, 4);
+    }
+
+}
+
+function drawSquareFilled(){
+    gl.drawArrays(gl.TRIANGLES, 0, vertices.length);
 }
